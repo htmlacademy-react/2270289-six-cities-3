8c7855a69f11907745dfe173fd96a23bcaff39d9
AistVolhv <istanik@ya.ru>
2024-11-25 14:56:48 +0500

    Приведение к JSX _ 2

--- src/main.tsx
+++ src/main.tsx
@@ -63,17 +63,17 @@ function App () {
               <b className='places__found'>312 places to stay in Amsterdam</b>
               <form className='places__sorting' action='#' method='get'>
                 <span className='places__sorting-caption'>Sort by</span>
-                <span className='places__sorting-type' tabIndex='0'>
+                <span className='places__sorting-type' tabIndex={0}>
                   Popular
                   <svg className='places__sorting-arrow' width='7' height='4'>
                     <use xlinkHref='#icon-arrow-select'></use>
                   </svg>
                 </span>
                 <ul className='places__options places__options--custom places__options--opened'>
-                  <li className='places__option places__option--active' tabIndex='0'>Popular</li>
-                  <li className='places__option' tabIndex='0'>Price: low to high</li>
-                  <li className='places__option' tabIndex='0'>Price: high to low</li>
-                  <li className='places__option' tabIndex='0'>Top rated first</li>
+                  <li className='places__option places__option--active' tabIndex={0}>Popular</li>
+                  <li className='places__option' tabIndex={0}>Price: low to high</li>
+                  <li className='places__option' tabIndex={0}>Price: high to low</li>
+                  <li className='places__option' tabIndex={0}>Top rated first</li>
                 </ul>
               </form>
               <div className='cities__places-list places__list tabs__content'>
@@ -265,25 +265,25 @@ function App () {
         </div>
       </main>
     </div>
-
-
   )
 }
 
 function ChoiceDirection() {
   return (
-    directions.map((item) => {
-      <li className='locations__item'>
-        <a className='locations__item-link tabs__item' href='#'>
-          <span>{item}</span>
-        </a>
-      </li>
-    }
-    )
+    <React.Fragment>
+      {directions.map((item) => {
+        <li className='locations__item'>
+          <a className='locations__item-link tabs__item' href='#'>
+            <span>{item}</span>
+          </a>
+        </li>
+      }
+      ).join()
+      }
+    </React.Fragment>
   )
 }
 
-
 root.render(
-  <App />
+    <App />
 );
