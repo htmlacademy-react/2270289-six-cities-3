1a3dddf0195b43a2e076fd8b2f2f455c58609c97
AistVolhv <istanik@ya.ru>
2024-11-26 16:55:16 +0500

    Исправление

--- src/components/offer.tsx
+++ src/components/offer.tsx
@@ -219,7 +219,7 @@ export default function Offer() : JSX.Element {
                     <p className="reviews__help">
                       To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
                     </p>
-                    <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
+                    <button className="reviews__submit form__submit button" type="submit" disabled = {false}>Submit</button>
                   </div>
                 </form>
               </section>
