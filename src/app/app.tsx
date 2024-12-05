97015bd808f4cfe967ceba759fbe69e0460b4efb
AistVolhv <istanik@ya.ru>
2024-12-05 16:27:36 +0500

    Добавили приватный маршрут.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,11 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
-import {AppRoute} from '../../src/const.ts';
+import {AppRoute,AuthorizationStatus} from '../../src/const.ts';
 import Main from '../pages/main/main';
 import Login from '../pages/login/login.tsx';
 import Offer from '../pages/offer/offer.tsx';
 import Favorites from '../pages/favorites/favorites.tsx';
 import Page404 from '../pages/404/page-404.tsx';
+import PrivateRoute from '../components/private-route/private-route.tsx';
 
 type AppCountArendaProps = {
   countArenda : number;
@@ -17,7 +18,15 @@ export default function App ({countArenda}: AppCountArendaProps) : JSX.Element {
         <Route path={AppRoute.Main} element={<Main countArenda = {countArenda} />}/>
         <Route path={AppRoute.Login} element={<Login />}/>
         <Route path={AppRoute.Offer} element={<Offer />}/>
-        <Route path={AppRoute.Favorites} element={<Favorites />}/>
+        <Route path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+              >
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
         <Route path={AppRoute.Page404} element={<Page404 />}/>
       </Routes>
     </BrowserRouter>
