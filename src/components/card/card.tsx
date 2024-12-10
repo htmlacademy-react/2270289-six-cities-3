c1a5169a5d73ecf147bd2a55449308b4fbcf4a0f
AistVolhv <istanik@ya.ru>
2024-12-10 11:15:28 +0500

    Разделение компонента списка карточек предложение на картучку и список из карточек.

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -62,46 +62,53 @@ type ListPreviewCardProps = {
   listPreviewCards: OfferPreviewCard[];
 }
 
-export default function Card({listPreviewCards}:ListPreviewCardProps) : JSX.Element {
+function CardOffer ({card} : {card: OfferPreviewCard}) : JSX.Element {
+  return (
+    <article className="cities__card place-card" key = {card.id}>
+      <div className="place-card__mark">
+        <span>
+          {card.isPremium ? 'Premium' : ''}
+        </span>
+      </div>
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={card.previewImage} width="260" height="200" alt="Place image" />
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{card.price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className="place-card__bookmark-button button" type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: '80%'}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{card.title}</a>
+        </h2>
+        <p className="place-card__type">{card.type}</p>
+      </div>
+    </article>
+  )
+}
+
+export default function ListOffer({listPreviewCards}:ListPreviewCardProps) : JSX.Element {
   return (
     <>
-      {listPreviewCards.map((card) => (
-        <article className="cities__card place-card" key = {card.id}>
-          <div className="place-card__mark">
-            <span>
-              {card.isPremium ? 'Premium' : ''}
-            </span>
-          </div>
-          <div className="cities__image-wrapper place-card__image-wrapper">
-            <a href="#">
-              <img className="place-card__image" src={card.previewImage} width="260" height="200" alt="Place image" />
-            </a>
-          </div>
-          <div className="place-card__info">
-            <div className="place-card__price-wrapper">
-              <div className="place-card__price">
-                <b className="place-card__price-value">&euro;{card.price}</b>
-                <span className="place-card__price-text">&#47;&nbsp;night</span>
-              </div>
-              <button className="place-card__bookmark-button button" type="button">
-                <svg className="place-card__bookmark-icon" width="18" height="19">
-                  <use xlinkHref="#icon-bookmark"></use>
-                </svg>
-                <span className="visually-hidden">To bookmarks</span>
-              </button>
-            </div>
-            <div className="place-card__rating rating">
-              <div className="place-card__stars rating__stars">
-                <span style={{width: '80%'}}></span>
-                <span className="visually-hidden">Rating</span>
-              </div>
-            </div>
-            <h2 className="place-card__name">
-              <a href="#">{card.title}</a>
-            </h2>
-            <p className="place-card__type">{card.type}</p>
-          </div>
-        </article>
+      {listPreviewCards.map((cardItem) => (
+        <CardOffer card = {cardItem}/>
+
       )
       )}
     </>
