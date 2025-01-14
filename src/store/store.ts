47a14a007ca2e2c384f739932891acf42ebde603
AistVolhv <istanik@ya.ru>
2025-01-14 16:59:24 +0500

    ...

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { createStore } from "react-redux";
 import { reducer,reducerReduxDefault } from "./reducer";
 //import { initialState } from "./reducer";
 
+/*
 export const storeDefaultRedux = configureStore({
   reducer: reducerReduxDefault
 })
@@ -9,9 +10,13 @@ export const storeDefaultRedux = configureStore({
 export const store = configureStore({
   reducer: reducer
 })
-
+*/
 
 // Функция для обновления хранилища
 const updateStore = (state, action) => {
   // Код функции
 };
+
+
+export const store = createStore(updateStore,0);
+
