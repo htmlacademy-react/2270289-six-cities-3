904bbc70a4aab8e629e607b8d8b7fee7ba8c5211
AistVolhv <istanik@ya.ru>
2025-01-17 15:22:08 +0500

    Новый компонент SortOffer

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,7 +1,8 @@
-import {useState} from 'react';
+//import {useState} from 'react';
 import {useAppSelector } from '../../hooks/index.ts';
 import Header from '../../components/header/header.tsx';
 import ListOffer from '../../components/card-offer-list/card-offer-list.tsx';
+import SortOffer from '../../components/sort-offer/sort-offer.tsx';
 import Map from '../../components/map/map.tsx';
 import ListCity from '../../components/list-city/list-city.tsx';
 
@@ -14,7 +15,7 @@ export default function Main () : JSX.Element {
   const countOffers = currentOffersByCity.length;
 
   //const [cardActiveId, setCardActiveId] = useState<string|null>(null);
-  const cardActiveId = currentOffersByCity[0].id;
+  //const cardActiveId = currentOffersByCity[0].id;
 
   return (
     <div className="page page--gray page--main">
@@ -31,21 +32,9 @@ export default function Main () : JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{countOffers} places to stay in {cityName}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
+
+              <SortOffer />
+
               <div className="cities__places-list places__list tabs__content">
 
                 <ListOffer listOffer={currentOffersByCity} variantCard='cities'  />
