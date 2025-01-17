46d7e84d625cfbabd44f33de4c715520de1ac43b
AistVolhv <istanik@ya.ru>
2025-01-17 10:12:33 +0500

    Новый компонент Header. Изменение компонента на listCity

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,9 +1,9 @@
-import {Link } from 'react-router-dom';
 import {useState} from 'react';
 import {useAppSelector } from '../../hooks/index.ts';
+import Header from '../../components/header/header.tsx';
 import ListOffer from '../../components/card-offer-list/card-offer-list.tsx';
 import Map from '../../components/map/map.tsx';
-import MainCityMenu from '../../components/main-city-menu/main-city-menu.tsx';
+import MainCityMenu from '../../components/list-city/list-city.tsx';
 
 export default function Main () : JSX.Element {
 
@@ -17,34 +17,8 @@ export default function Main () : JSX.Element {
 
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link className="header__logo-link header__logo-link--active" to='/'>
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+
+      <Header />
 
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
