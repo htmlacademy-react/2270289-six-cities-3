2a44e2b36f594670c8972af2d76b5923d63928d1
AistVolhv <istanik@ya.ru>
2025-01-21 16:53:08 +0500

    Проба функции для сортировки.

--- src/components/card-offer-list/card-offer-list.tsx
+++ src/components/card-offer-list/card-offer-list.tsx
@@ -8,13 +8,12 @@ type ListOfferProps = {
   variantCard : 'cities'|'favorite'|'near-places';
 }
 
-export default function ListOffer({listOffer,variantCard} : ListOfferProps) : JSX.Element {
-  const currentSort = useAppSelector((state) => state.currentSort);
+const getSortedListOffer = (currentSort: string, listOffer: OfferPreview[]) => {
   const sortedListOffer = listOffer;
 
   switch (currentSort) {
-    //case (SORT_OPTIONS[SortOption.Popular]): sortedListOffer;
-    //break;
+    case (SORT_OPTIONS[SortOption.Popular]): return [...listOffer];
+      break;
     case (SORT_OPTIONS[SortOption.PriceHighToLow]): sortedListOffer.sort((a,b) => b.price - a.price);
       break;
     case (SORT_OPTIONS[SortOption.PriceLowToHigh]): sortedListOffer.sort((a,b) => a.price - b.price);
@@ -22,6 +21,14 @@ export default function ListOffer({listOffer,variantCard} : ListOfferProps) : JS
     case (SORT_OPTIONS[SortOption.TopRatedFirst]): sortedListOffer.sort((a,b) => b.rating - a.rating);
       break;
   }
+
+  return sortedListOffer;
+
+};
+
+export default function ListOffer({listOffer,variantCard} : ListOfferProps) : JSX.Element {
+  const currentSort = useAppSelector((state) => state.currentSort);
+  const sortedListOffer = getSortedListOffer(currentSort,listOffer);
 
   return (
     <>
