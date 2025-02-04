f3349eff10aac3c153c187610b0f16b4b18d818d
AistVolhv <istanik@ya.ru>
2025-02-04 16:39:16 +0500

    правки

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useRef, FormEvent } from 'react';
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../../hooks';
 import { loginAction } from '../../store/api-actions';
 //import { AppRoute } from '../../const';
