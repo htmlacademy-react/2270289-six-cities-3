8a1dc712dcff53bd591644dd3dd846fe03ce98df
AistVolhv <istanik@ya.ru>
2025-02-05 16:22:55 +0500

    Иправление ссылок.

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,6 +1,8 @@
 import FavoriteCardItemGroup from '../../components/favorite-card-item-group/favorite-card-item-group.tsx';
 import type {OfferPreview} from '../../types.ts';
 import { useAppSelector } from '../../hooks/index.ts';
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const.ts';
 
 type ListOfferProps = {
   variantCard : 'cities'|'favorite'|'near-places';
@@ -16,19 +18,19 @@ export default function Favorites({ variantCard}:ListOfferProps) : JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
+              <Link className="header__logo-link" to={AppRoute.Main}>
                 <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              </Link>
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
+                  <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Main}>
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
                     <span className="header__favorite-count">3</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="header__nav-item">
                   <a className="header__nav-link" href="#">
@@ -54,9 +56,9 @@ export default function Favorites({ variantCard}:ListOfferProps) : JSX.Element {
         </div>
       </main>
       <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
+        <Link className="footer__logo-link" to={AppRoute.Main}>
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-        </a>
+        </Link>
       </footer>
     </div>
   );
