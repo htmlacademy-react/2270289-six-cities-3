37af15ce71724e4d801b3c72b7e5cec42aeebe27
AistVolhv <istanik@ya.ru>
2025-02-18 09:45:52 +0500

    Очищение статуса ошибки при переходе на главную.

--- src/pages/404/page-404.tsx
+++ src/pages/404/page-404.tsx
@@ -1,9 +1,16 @@
 import {Link} from 'react-router-dom';
+import { useAppDispatch } from '../../hooks';
+import { setError } from '../../store/action';
+import { errorEmpty } from '../../const';
 
+const clearErrorStatus = () => {
+  const dispatch = useAppDispatch();
+  dispatch(setError(errorEmpty));
+}
 
 export default function Page404 () {
   return (
-    <Link to='/'>
+    <Link to='/' onClick={clearErrorStatus}>
       <div style={{textAlign : 'center'}}>
         <img src="img/404.jpg"/>
         <p>Вернуться на главную страницу!</p>
