4c2ec3ca77aae1ddb7de0cfacee6c145d22195cc
AistVolhv <istanik@ya.ru>
2025-03-06 09:29:30 +0500

    Ghfdrb

--- src/services/process-error-handle.ts
+++ src/services/process-error-handle.ts
@@ -1,12 +1,12 @@
 
-import {store} from '../store';
-import {setError} from '../store/action';
-import {clearErrorAction} from '../store/api-actions';
-import type { TErrorRequest } from '../types';
+// import {store} from '../store';
+// import {setError} from '../store/action';
+// import {clearErrorAction} from '../store/api-actions';
+// import type { TErrorRequest } from '../types';
 
-export const processErrorHandle = (errorRequest: TErrorRequest): void => {
-  store.dispatch(setError(errorRequest));
-  if (errorRequest.status !== 404) {
-    store.dispatch(clearErrorAction());
-  }
-};
+// export const processErrorHandle = (errorRequest: TErrorRequest): void => {
+//   store.dispatch(setError(errorRequest));
+//   if (errorRequest.status !== 404) {
+//     store.dispatch(clearErrorAction());
+//   }
+// };
