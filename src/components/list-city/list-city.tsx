2b347fb97f4924c7a06b8bd5508ea5d9d0dc1af3
AistVolhv <istanik@ya.ru>
2025-03-12 13:59:21 +0500

    Реализация того, чтоб текущий город выделялся в списке городов.

--- src/components/list-city/list-city.tsx
+++ src/components/list-city/list-city.tsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const.ts';
-import { useAppDispatch } from '../../hooks/index.ts';
+import { useAppDispatch, useAppSelector } from '../../hooks/index.ts';
 import { citiesList } from '../../const.ts';
 import { setCurrentCity } from '../../store/all-offers/all-offers.slice.ts';
 import { memo } from 'react';
+import { currentCity } from '../../store/all-offers/all-offers.selectors.ts';
 
 function ListCity(): JSX.Element {
   const dispatch = useAppDispatch();
+  const activeCity = useAppSelector(currentCity);
 
   return (
     <div className="tabs">
@@ -15,10 +17,13 @@ function ListCity(): JSX.Element {
           {
             citiesList.map((item) => (
               <li className="locations__item" key={item.name} >
-                <Link className="locations__item-link tabs__item" to={AppRoute.Main} onClick={(evt) => {
-                  evt.preventDefault();
-                  dispatch(setCurrentCity(item));
-                }}
+                <Link
+                  className={`locations__item-link tabs__item ${item === activeCity && 'tabs__item--active'}`}
+                  to={AppRoute.Main}
+                  onClick={(evt) => {
+                    evt.preventDefault();
+                    dispatch(setCurrentCity(item));
+                  }}
                 >
                   <span>{item.name}</span>
                 </Link>
